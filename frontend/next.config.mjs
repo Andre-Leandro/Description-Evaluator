8c895ebef93c4215650bc5376725b5ed775c363c
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2025-07-23 00:04:33 -0300

    fix requiere

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,11 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     };
     return config;
   },
