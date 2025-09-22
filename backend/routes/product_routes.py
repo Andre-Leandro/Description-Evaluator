ed11e3462cd009a21a1a2a9026ee540ea21dbe23
SixtoArrejin <arrejinsixto@gmail.com>
2025-09-22 11:01:17 -0300

    ult intento azure

--- backend/routes/product_routes.py
+++ backend/routes/product_routes.py
@@ -8,6 +8,7 @@ import os
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST", "127.0.0.1"),
     port=int(os.getenv("REDIS_PORT", 6379)),
+    password=os.getenv("REDIS_PASSWORD", None),
 )
 
 
